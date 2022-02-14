5ebd6907df6eadfff21c20fac77fd1d61330cce7
Marlon Dauernheimer <marlon.dauernheimer@gmail.com>
2022-02-14 20:14:13 -0300

    adjusted set values and test to cryptor

--- src/main/java/br/com/challenges/fifth/model/CryptorCipher.java
+++ src/main/java/br/com/challenges/fifth/model/CryptorCipher.java
@@ -1,6 +1,7 @@
 package br.com.challenges.fifth.model;
 
 import java.util.Base64;
+import java.util.Objects;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -20,6 +21,9 @@ public class CryptorCipher implements Cryptor {
 	}
 
 	public String encrypt(String text) throws EncryptionException {
+		if (Objects.isNull(text) || text.trim().isEmpty())
+			throw new IllegalArgumentException("Deve ser informado um texto!");
+
 		try {
 		    return Base64.getEncoder().encodeToString(encrypt.doFinal(text.getBytes()));
 		} catch (IllegalBlockSizeException | BadPaddingException exception) {
@@ -28,6 +32,9 @@ public class CryptorCipher implements Cryptor {
 	}
 
 	public String decrypt(String text) throws DecryptionException {
+		if (Objects.isNull(text) || text.trim().isEmpty())
+			throw new IllegalArgumentException("Deve ser informado um texto!");
+
 		try {
 		    return new String(decrypt.doFinal(Base64.getDecoder().decode(text)));
 		} catch (IllegalBlockSizeException | BadPaddingException exception) {
--- src/test/java/br/com/challenges/fifth/FifthTest.java
+++ src/test/java/br/com/challenges/fifth/FifthTest.java
@@ -1,5 +1,65 @@
 package br.com.challenges.fifth;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.challenges.fifth.exception.CreateCryptorException;
+import br.com.challenges.fifth.exception.DecryptionException;
+import br.com.challenges.fifth.exception.EncryptionException;
+import br.com.challenges.fifth.exception.GenerateSecretException;
+import br.com.challenges.fifth.model.Cryptor;
+
 public class FifthTest {
+
+	private Cryptor cryptor;
+
+	@Before
+	public void init() {
+		try {
+			Fifth fifth = new Fifth();
+			this.cryptor = fifth.execute();
+		} catch (GenerateSecretException | CreateCryptorException exception) {
+			throw new RuntimeException("Erro ao criar o cryptor!");
+		}
+	}
+
+	@Test
+	public void encrypt_null_value() throws EncryptionException {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> { this.cryptor.encrypt(""); });
+		assertEquals("Deve ser informado um texto!", exception.getMessage());
+	}
+
+	@Test
+	public void encrypt_empty_value() throws EncryptionException {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> { this.cryptor.encrypt(""); });
+		assertEquals("Deve ser informado um texto!", exception.getMessage());
+	}
+
+	@Test
+	public void encrypt_value() throws EncryptionException {
+		String encrypted = this.cryptor.encrypt("super-secret-info");
+		assertEquals("UEKwWm4YFGcnkVJ4ebhSii1LAs7V2Ivx2Gm+/QxUpro=", encrypted);
+	}
+
+	@Test
+	public void decrypt_null_value() throws DecryptionException {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> { this.cryptor.decrypt(null); });
+		assertEquals("Deve ser informado um texto!", exception.getMessage());
+	}
+
+	@Test
+	public void decrypt_empty_value() throws DecryptionException {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> { this.cryptor.decrypt(""); });
+		assertEquals("Deve ser informado um texto!", exception.getMessage());
+	}
+
+	@Test
+	public void decrypt_value() throws DecryptionException {
+		String decrypted = this.cryptor.decrypt("UEKwWm4YFGcnkVJ4ebhSii1LAs7V2Ivx2Gm+/QxUpro=");
+		assertEquals("super-secret-info", decrypted);
+	}
 
 }
